356eba52172d7ed7eb956143822ebf5697dbcf10
Jjenny-K <kkjjh0129@gmail.com>
2022-04-28 11:47:47 +0900

    (feat)db uploader

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+DATA_URL = "/data/"
+DATA_ROOT = os.path.join(BASE_DIR, 'data')
+DATA_ROOT_URL = '.'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
