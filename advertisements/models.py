14c570bc7eae10f9668fbb7a1d6ca4fe6e9e0fb4
leeceo97 <leeceo97@naver.com>
2022-04-28 10:40:15 +0900

    (fix)modeling

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Advertisement(models.Model):
-    advertisement_uid = models.CharField(max_length=65, unique=True)
+    advertisement_uid = models.CharField(max_length=65, primary_key=True)
     advertiser = models.ForeignKey('advertisers.Advertiser', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.uid
+        return self.advertisement_uid
 
 class AdvertisementInfo(models.Model):
     advertisement = models.ForeignKey('advertisements.Advertisement', on_delete=models.CASCADE)
@@ -21,7 +21,7 @@ class AdvertisementInfo(models.Model):
         KAKAO = 'kakao'
         GOOGLE = 'google'
         FACEBOOK = 'facebook'
-    media = models.CharField(
+    name = models.CharField(
         max_length=15,
         choices=MediaType.choices
     )
